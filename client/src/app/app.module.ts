22ec3abf535dba5752248868a5b856f3ddc18f47
JonSuntkd <jonatansuntaxitkd@gmail.com>
2020-09-08 23:00:08 -0500

    exa3

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { GameFormComponent } from './components/game-form/game-form.component';
 import { GameListComponent } from './components/game-list/game-list.component';
+
+
+// Services
+import { GamesService } from './services/games.service';
 
 @NgModule({
   declarations: [
@@ -18,7 +26,9 @@ import { GameListComponent } from './components/game-list/game-list.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+     GamesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ client/src/app/models/Game.ts
@@ -0,0 +1,10 @@
+
+import { Title } from '@angular/platform-browser';
+
+export interface Game {
+    COD_SUB_CATEGORIA?: string,
+    COD_CATEGORIA?: string,
+    NOMBRE?: string,
+    DESCRIPCION?: string,
+    FECHA_CREACION?: Date
+};
